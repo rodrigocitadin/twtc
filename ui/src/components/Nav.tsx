f820e7423215dfa9fdbb9adc03c6895739e29d05
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-07 15:13:27 -0300

    wip

--- ui/src/components/Nav.tsx
+++ ui/src/components/Nav.tsx
@@ -3,17 +3,14 @@ import { Link } from "react-router-dom"
 
 export default function Nav() {
   return (
-    <nav className="hover:*:bg-gray-200 *:py-2 *:px-4 *:rounded-3xl px-8 pt-12 flex flex-col gap-2 *:flex *:gap-2 *:items-center *:text-xl border-r-2 border-r-gray-100">
+    <nav className="min-h-screen hover:*:bg-gray-200 *:py-2 *:px-4 *:rounded-3xl px-8 pt-12 flex flex-col items-start gap-2 *:flex *:gap-2 *:items-center *:text-xl border-r-2 border-r-gray-100">
       <Link to="/"><Home /> Home</Link>
       <Link to="/explore"><ExploreOutlined /> Explore</Link>
       <Link to="/notifications"><NotificationsOutlined /> Notifications</Link>
       <Link to="/messages"><EmailOutlined /> Messages</Link>
       <Link to="/lists"><ArticleOutlined /> Lists</Link>
       <Link to="/bookmarks"><BookmarkBorderOutlined />Bookmarks</Link>
-      {/* <Link to="/communities">Communities</Link> */}
-      {/* <Link to="/premium">Premium</Link> */}
       <Link to="/profile"><AccountBoxOutlined />Profile</Link>
-      {/* <Link to="/more">More</Link> */}
     </nav>
   )
 }
