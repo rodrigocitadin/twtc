fcece2d0756ce0c1394b1838e7ce56126a71ea56
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-07 01:45:44 -0300

    start nav

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
-    <h1>
-      Home
-    </h1>
+    <main>
+      <nav className="flex flex-col gap-2">
+        <Link to="/">Home</Link>
+        <Link to="/explore">Explore</Link>
+        <Link to="/notifications">Notifications</Link>
+        <Link to="/messages">Messages</Link>
+        <Link to="/lists">Lists</Link>
+        <Link to="/bookmarks">Bookmarks</Link>
+        <Link to="/communities">Communities</Link>
+        <Link to="/premium">Premium</Link>
+        <Link to="/profile">Profile</Link>
+        {/* <Link to="/more">More</Link> */}
+      </nav>
+    </main>
   )
 }
