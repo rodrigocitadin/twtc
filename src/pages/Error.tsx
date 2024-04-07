d42e86a98f20cf38d63bf5604166feb8b9e408c4
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-06 22:14:41 -0300

    fix error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,14 +1,21 @@
-import { useRouteError } from "react-router-dom"
+import { isRouteErrorResponse, useRouteError } from "react-router-dom"
 
 export default function Error() {
-  const error: any = useRouteError();
+  const error = useRouteError()
+  let errorMessage
+  let errorStatus
+
+  if (isRouteErrorResponse(error)) {
+    errorMessage = error.statusText
+    errorStatus = error.status
+  }
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{`${errorMessage} - ${errorStatus}`}</i>
       </p>
     </div>
   )
