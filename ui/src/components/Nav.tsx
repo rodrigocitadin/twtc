42adeef51879d9d9fbff27415d8d3a1e712caaac
rodrigocitadin <citadinrodrigo@gmail.com>
2024-04-08 23:04:55 -0300

    much much much MUCH better

--- ui/src/components/Nav.tsx
+++ ui/src/components/Nav.tsx
@@ -7,44 +7,34 @@ import ProfileIcon from "../icons/ProfileIcon";
 import { useState } from "react";
 
 export default function Nav() {
-  const [activeButton, setActiveButton] = useState({
-    home: true,
-    explore: false,
-    notifications: false,
-    messages: false,
-    profile: false
-  })
+  const buttons = [
+    { href: "/", icon: HomeIcon, isActive: true, text: "Home" },
+    { href: "/explore", icon: ExploreIcon, isActive: false, text: "Explore" },
+    { href: "/notifications", icon: NotificationsIcon, isActive: false, text: "Notifications" },
+    { href: "/messages", icon: MessagesIcon, isActive: false, text: "Messages" },
+    { href: "/profile", icon: ProfileIcon, isActive: false, text: "Profile" },
+  ]
 
-  const defaultState = {
-    home: false,
-    explore: false,
-    notifications: false,
-    messages: false,
-    profile: false
-  }
+  const [pathname, setPathname] = useState(window.location.pathname)
 
   return (
     <nav className="min-h-screen hover:*:bg-gray-200 *:py-2 *:px-4 *:rounded-3xl px-8 pt-12 flex flex-col items-start gap-2 *:flex *:gap-2 *:items-center *:text-xl border-r-2 border-r-gray-100">
-      <Link onClick={() => setActiveButton({ ...defaultState, home: true })} to="/">
-        <HomeIcon isActive={activeButton.home} />
-        Home
-      </Link>
-      <Link onClick={() => setActiveButton({ ...defaultState, explore: true })} to="/explore">
-        <ExploreIcon isActive={activeButton.explore} />
-        Explore
-      </Link>
-      <Link onClick={() => setActiveButton({ ...defaultState, notifications: true })} to="/notifications">
-        <NotificationsIcon isActive={activeButton.notifications} />
-        Notifications
-      </Link>
-      <Link onClick={() => setActiveButton({ ...defaultState, messages: true })} to="/messages">
-        <MessagesIcon isActive={activeButton.messages} />
-        Messages
-      </Link>
-      <Link onClick={() => setActiveButton({ ...defaultState, profile: true })} to="/profile">
-        <ProfileIcon isActive={activeButton.profile} />
-        Profile
-      </Link>
+      {
+        buttons.map((v, i) => {
+          const LinkIcon = v.icon
+
+          return (
+            <Link 
+              key={i} 
+              to={v.href}
+              onClick={() => setPathname(v.href)}
+            >
+              <LinkIcon isActive={pathname === v.href} />
+              {v.text}
+            </Link>
+          )
+        })
+      }
     </nav>
   )
 }
